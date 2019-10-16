891c6453f37ed1f0fc4d38ce65c9016efeee0fe6
francisco-fernandez <paco.fernandez@gmx.net>
2019-10-16 23:58:06 +0200

    Updates

---
+++ precache-manifest.fabc2c5a2c7623b18f0d1e9083baf1bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8f780a9166c3c0a05d0d4895e0aeab",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4194ddea0f16494719ab",
+    "url": "/monster-rolodex/static/css/main.b7a5d4db.chunk.css"
+  },
+  {
+    "revision": "2554629ff5be1b770d69",
+    "url": "/monster-rolodex/static/js/2.dd67a859.chunk.js"
+  },
+  {
+    "revision": "4194ddea0f16494719ab",
+    "url": "/monster-rolodex/static/js/main.122067e9.chunk.js"
+  },
+  {
+    "revision": "f75c77ac67bebe39c115",
+    "url": "/monster-rolodex/static/js/runtime-main.749f14a7.js"
+  }
+]);
